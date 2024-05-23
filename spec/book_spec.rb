1757004c745d0b83965549ecaac567f922b2e221
jospin6 <jospinndagano1@gmail.com>
2024-05-23 15:44:52 +0200

    Write unit tests for the books clas

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,5 +1,13 @@
 require_relative '../books'
 
 describe Books do
-    
+    context "it should create a book and add the title, author, published year and availability" do
+       book = Books.new('gemini', 'jn', '2023', true)
+       it 'create new book with the given parameter' do
+            expect(book).to be_an_instance_of Books  
+       end
+       it 'should return the book title' do
+            expect(book.title).to eql("gemini")  
+       end
+    end
 end
