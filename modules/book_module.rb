1c31f8dd00161549f103574a202c4068efd869e7
jospin6 <jospinndagano1@gmail.com>
2024-05-30 13:57:23 +0200

    Add load books methode to the books module

--- modules/book_module.rb
+++ modules/book_module.rb
@@ -4,6 +4,17 @@ require_relative '../books'
 module BookModule
     def load_books
         file = '../data_files/books.json'
+        data = []
+        if File.exist?(file) && File.read(file) != ''
+            JSON.parse(File.read(file)).each do |element|
+                data.push(Books.new(element['title'], element['author'], element['published_year'], element['isAvailable']))
+            end
+        end
+        data
+    end
+    
+    def save_book
+        data = []
     end
     
 end
