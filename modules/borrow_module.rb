f7b8de105199b7bc7e4f04b6abbe4844b1322ebc
jospin6 <jospinndagano1@gmail.com>
2024-05-31 21:52:53 +0200

    add save borrow method to the module

--- modules/borrow_module.rb
+++ modules/borrow_module.rb
@@ -12,5 +12,14 @@ module BorrowModule
         end
         data
     end
+
+    def save_borrow
+        data = []
+        @borrow.each do |element|
+            data.push({id: element.id, book_id: element.book_id, student_id: element.student_id, borrow_date: element.borrow_date, return_date: element.return_date})
+        end
+        File.write('../data_files/borrows.json', JSON.generate(data))
+    end
+    
     
 end
