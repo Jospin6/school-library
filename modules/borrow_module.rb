21f01d2e1499be3b3c938eac52fc45a5d9f9c96b
jospin6 <jospinndagano1@gmail.com>
2024-05-31 21:26:03 +0200

    Add load borrows method to the borrow module

--- modules/borrow_module.rb
+++ modules/borrow_module.rb
@@ -2,5 +2,15 @@ require 'json'
 require_relative '../borrow'
 
 module BorrowModule
-
+    def load_borrows
+        file = '../data_files/borrows.json'
+        data = []
+        if File.exist?(file) && File.read(file) != ''
+            JSON.parse(File.read(file)).each do |borrow|
+                data.push(Borrow.new(borrow['book_id'], borrow['student_id'], borrow['borrow_date'], borrow['return_date']))
+            end
+        end
+        data
+    end
+    
 end
