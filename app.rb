c4e85845046ebcfd2c3804dba6a27ededcf6f8d6
jospin6 <jospinndagano1@gmail.com>
2024-06-01 13:21:10 +0200

    List all the students

--- app.rb
+++ app.rb
@@ -17,8 +17,16 @@ class App
     def list_all_books
         puts "All of our Books"
         @books.each do |book|
-            puts "Book title: #{book.title}, author: #{book.author}, published year #{book.published_year}"
+            puts "ID #{book.id}, book title: #{book.title}, author: #{book.author}, published year #{book.published_year}"
         end
     end
+
+    def list_students
+        puts "Ours students"
+        @students.each do |student|
+           puts "ID #{student.id}, name: #{student.name}, last name: #{student.second_name}, degree: #{student.degree}" 
+        end
+    end
+    
     
 end
