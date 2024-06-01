cc1f4f73789649de12287e017f84c4b94b2be782
jospin6 <jospinndagano1@gmail.com>
2024-06-01 12:44:23 +0200

    Add load_ students method to the student module

--- modules/student_module.rb
+++ modules/student_module.rb
@@ -2,5 +2,15 @@ require 'json'
 require_relative '../students'
 
 module StudentModule
-
+    def load_students
+        file = '../data_file/students.json'
+        data = []
+        if File.exist?(file) && File.read(file) != ''
+            JSON.parse(File.read(file)).each do |student|
+                data.push(Students.new(student['name'], student['second_name'], student['degree']))
+            end
+        end
+        data
+    end
+    
 end
