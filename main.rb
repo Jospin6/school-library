76e52eab73e548f097be669946388192d5a9bd12
jospin6 <jospinndagano1@gmail.com>
2024-06-01 14:12:08 +0200

    Add menu to the main class

---
+++ main.rb
@@ -0,0 +1,36 @@
+require_relative './app'
+
+class Main
+    def initialize
+        @app = App.new
+    end
+
+    def run
+        puts '*************SCHOOL LIBRARY************'
+        puts '             =======================          '
+        puts 'Welcome to our school library!'
+        loop do
+          menu
+          option = gets.chomp
+          break if option == '7'
+    
+          @app.get_options option
+        end
+        @app.save_book
+        @app.save_student
+        @app.create_borrow
+        puts 'Thank you for using our Library!'
+    end
+
+    def menu
+        puts
+        puts 'Please choose an option by entering a number'
+        puts '1 - List all books'
+        puts '2 - List all students'
+        puts '3 - List all loans'
+        puts '4 - Add new book'
+        puts '5 - Add new student'
+        puts '6 - Add new loan'
+        puts '7 - Exit'
+    end
+end
